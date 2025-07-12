351f92370553b1414e3f01c085896b2ea66509da
Skandaking <skandakaunda@gmail.com>
2025-07-12 22:34:29 +0200

    removed  unused link from documents page

--- src/app/(public)/publications/documents/page.tsx
+++ src/app/(public)/publications/documents/page.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import { FileText, Download, Calendar, Search, Filter, Eye, File } from 'lucide-react';
 
 export default function DocumentsPage() {
