ad97e5eb1547cac5e3ce75ab4f30dadb6487d91e
Skandaking <skandakaunda@gmail.com>
2025-07-14 22:33:14 +0200

    added public gallery to navbar

--- src/components/public/shared/Navbar.tsx
+++ src/components/public/shared/Navbar.tsx
@@ -249,6 +249,17 @@ export default function Navbar() {
                       >
                         EFT Project
                       </Link>
+                      <Link
+                        href="/projects/public-gallery"
+                        className={cn(
+                          "block px-4 py-2 text-sm hover:bg-primary hover:text-white transition-colors",
+                          isActive("/projects/public-gallery")
+                            ? "text-primary font-bold"
+                            : "text-gray-700"
+                        )}
+                      >
+                        Public Gallery
+                      </Link>
                     </div>
                   </div>
                 );
@@ -575,6 +586,18 @@ export default function Navbar() {
                     )}
                   >
                     EFT Project
+                  </Link>
+                  <Link 
+                    href="/projects/public-gallery"
+                    onClick={closeMobileMenu}
+                    className={cn(
+                      "block px-4 py-2 rounded-md text-sm transition-colors",
+                      isActive("/projects/ipsas-implementation")
+                        ? "text-primary bg-primary/10 font-medium"
+                        : "text-gray-600 hover:text-primary hover:bg-gray-100"
+                    )}
+                  >
+                    Public Gallery
                   </Link>
                 </div>
               </div>
